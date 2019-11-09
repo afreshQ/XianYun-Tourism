9a26a467937ced4e4315987091665aa2bad146b8
afreshQ <1040044589@qq.com>
2019-11-09 16:05:29 +0800

    使用vuex里面的store架构存放登陆拿到的用户信息

---
+++ store/user.js
@@ -0,0 +1,13 @@
+export const state={
+    userInfo:{
+        token:'',
+        user:{}
+    }
+};
+
+
+export const mutations={
+    setUserInfo(state,data){
+        state.userInfo=data;
+    }
+}
