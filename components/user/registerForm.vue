86f2a9ebce7247c6ce7b65fa7be61cf5c6a0c2d5
afreshQ <1040044589@qq.com>
2019-11-11 16:01:13 +0800

    注册功能实现

---
+++ components/user/registerForm.vue
@@ -0,0 +1,195 @@
+<template>
+    <el-form 
+        :model="form" 
+        ref="form" 
+        :rules="rules" 
+        class="form">
+            <el-form-item class="form-item" prop="username">
+                <el-input 
+                placeholder="用户名手机"
+                v-model="form.username"
+                >
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="captcha">
+                <el-input 
+                v-model="form.captcha"
+                placeholder="验证码" >
+                    <template slot="append">
+                        <el-button @click="handleSendCaptcha">
+                            发送验证码
+                        </el-button>
+                    </template>
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="nickname">
+                <el-input 
+                v-model="form.nickname"
+                placeholder="你的名字">
+                </el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="password">
+                <el-input 
+                placeholder="密码" 
+                type="password"
+                v-model="form.password"
+                ></el-input>
+            </el-form-item>
+
+            <el-form-item class="form-item" prop="checkPassword">
+                <el-input 
+                placeholder="确认密码" 
+                v-model="form.checkPassword"
+                type="password">
+                </el-input>
+            </el-form-item>
+
+            <el-button 
+            class="submit" 
+            type="primary" 
+            @click="handleRegSubmit">
+                注册
+            </el-button>
+        </el-form>
+</template>
+
+<script>
+export default {
+    data(){
+        
+        var checkPass = (rule, value, callback) => {
+            if (value === '') {
+            callback(new Error('请再次输入密码'));
+            } else if (value !== this.form.password) {
+            callback(new Error('两次输入密码不一致!'));
+            } else {
+            callback();
+            }
+        };
+        return {
+            // 表单数据
+            form: {
+                username:'',
+                nickname:'',
+                captcha:'',
+                password:'',
+                checkPassword: '',
+            },
+            // 表单规则
+            rules: {
+                username : [
+                        { required: true, message: '请输入用户名/手机', trigger: 'blur' },
+                ],
+                nickname: [
+                        { required: true, message: '请输入昵称', trigger: 'blur' },
+                ],
+                captcha : [
+                        { required: true, message: '请输入短信验证码', trigger: 'blur' },
+                ],
+                password: [
+                        { required: true, message: '请输入密码', trigger: 'blur' },
+                ],
+                checkPassword: [
+                        { validator: checkPass, trigger: 'blur' },
+                ],
+            },
+        }
+    },
+    methods: {
+        // 发送验证码
+        handleSendCaptcha(){
+            if(!this.form.username){
+                this.$confirm('手机号码不能为空', '提示', {
+                    confirmButtonText: '确定',
+                    showCancelButton: false,
+                    type: 'warning'
+                })
+                return;
+            }
+
+            if(this.form.username.length !== 11){
+                this.$confirm('手机号码格式错误', '提示', {
+                    confirmButtonText: '确定',
+                    showCancelButton: false,
+                    type: 'warning'
+                })
+                return;
+            }
+        
+            this.$axios({
+                url: '/captchas',
+                method: "POST",
+                data: {
+                    tel: this.form.username
+                }
+            }).then(res => {
+                const {code} = res.data;
+                this.$confirm(`模拟手机验证码为：${code}`, '提示', {
+                    confirmButtonText: '确定',
+                    showCancelButton: false,
+                    type: 'warning'
+                });
+            })
+
+           
+        },
+
+
+        // 注册
+        handleRegSubmit(){
+
+            //不需要checkPassword
+            let {checkPassword,...props}=this.form;
+            console.log(props);
+            
+           this.$refs.form.validate((vaild)=>{
+               if(vaild){
+                   this.$axios({
+                       url:'/accounts/register',
+                       method:'post',
+                       data:props
+                   }).then(res=>{
+                       console.log(res.data);
+                        this.$message({
+                            message: '注册成功',
+                            type: 'success'
+                        });
+                   }).catch(res=>{
+                       const {message}=res.response.data
+                       console.log(message);
+                       this.$message({
+                            message: message,
+                            type: 'warning'
+                        });
+                   })
+               }
+           })
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+    .form{
+        padding:25px;
+    }
+
+    .form-item{
+        margin-bottom:20px;
+    }
+
+    .form-text{
+        font-size:12px;
+        color:#409EFF;
+        text-align: right;
+        line-height: 1;
+    }
+
+    .submit{
+        width:100%;
+        margin-top:10px;
+    }
+</style>
