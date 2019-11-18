8b419a03b4e3e545719d1e7b1568ae472dfd64ae
afreshQ <1040044589@qq.com>
2019-11-18 21:14:16 +0800

    增加评论模块布局

--- pages/post/detialPost.vue
+++ pages/post/detialPost.vue
@@ -11,6 +11,7 @@
             </div>
             <!-- 文章详情渲染 -->
             <postRender :postData="postData"/>
+            <!-- 用户功能 -->
              <div class="post-bar">
                 <div class="icon"><i class="el-icon-chat-line-square"></i><p>评论({{postData.comments.length | zerofill}})</p></div>
                 <div class="icon"
@@ -20,6 +21,41 @@
                 <div class="icon"
                 @click="handleLike"
                 ><i class="el-icon-thumb"></i><p>点赞({{postData.like | zerofill}})</p></div>
+            </div>
+
+            <!-- 文章评论 -->
+            <div class="post-comment">
+                <h4 class="comment-title">评论</h4>
+                <el-input
+                class="input"
+                type="textarea"
+                :rows="2"
+                placeholder="说点什么把..."
+                v-model="textarea"
+                resize="none">
+                </el-input>
+                <el-row type="flex" justify="space-between" class="img-upload">
+                    <el-col>
+                        <el-upload
+                        action="https://jsonplaceholder.typicode.com/posts/"
+                        list-type="picture-card"
+                        :on-preview="handlePictureCardPreview"
+                        :on-remove="handleRemove">
+                        <i class="el-icon-plus"></i>
+                        </el-upload>
+                        <el-dialog :visible.sync="dialogVisible">
+                            <img width="100%" class="upload-img" :src="dialogImageUrl" alt="">
+                        </el-dialog>
+                    </el-col>
+                    <el-col :span="3">
+                          <el-button type="primary">提交</el-button>
+                    </el-col>
+                </el-row>
+                <div class="no-comments" v-if="postData.comments.length===0">
+                                    暂无评论，赶紧抢占沙发！
+                </div>
+                <postComment :postId="postId" v-else/>
+
             </div>
         </el-col>
         <el-col :span="7">
@@ -31,20 +67,28 @@
 
 <script>
 import postRender from '@/components/post/postRender';
+import postComment from '@/components/post/postComment';
 export default {
     components:{
-        postRender
+        postRender,
+        postComment
     },
     data(){
         return {
+            //该文章id
+            postId:null,
+            //文章数据
             postData:{
-                
                 city:{},
-                comments:[]
+                comments:[],
+                account:{}
             },
 
-            //该文章id
-            postId:null
+            textarea:'',
+
+            //图片上传组件
+            dialogImageUrl: '',
+            dialogVisible: false
         }
     },
     mounted(){
@@ -81,6 +125,7 @@ export default {
             })
         },
 
+        //点赞文章
         handleLike(){
              this.$axios({
                 url:'/posts/like',
@@ -128,6 +173,32 @@ export default {
             font-size: 14px;
             margin-top: 5px;
         }
+    }
+}
+
+.post-comment{
+    .comment-title{
+        font-size: 18px;
+        font-weight: normal;
+        margin-bottom: 20px;
+    }
+    .input{
+        margin-bottom: 10px;
+    }
+    .img-upload{
+        margin-bottom: 30px;
+    }
+    /deep/.el-upload{
+        width: 100px;
+        height: 100px;
+        line-height: 100px;
+    }
+    .no-comments{
+        font-size: 14px;
+        color: #999;
+        padding: 30px;
+        text-align: center;
+        border: 1px dashed #dddddd;
     }
 }
 </style>
