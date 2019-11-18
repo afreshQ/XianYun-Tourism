da5989c227965ca1ded9e0e1aba462a6f6a09ca5
afreshQ <1040044589@qq.com>
2019-11-18 10:33:12 +0800

    文章列表完成

---
+++ components/post/postList.vue
@@ -0,0 +1,157 @@
+<template>
+   <div>
+    <el-row class="post-item" type="flex" justify="space-between" v-if="data.images.length<3">
+        <el-col :span="8">
+            <img class="item-img" :src="data.images.length>0?data.images[0]:'/img/default.jpg'" alt="">
+        </el-col>
+        <el-col :span="16">
+            <div class="item-text">
+                <h4 class="item-title"><nuxt-link to="#">{{data.title}}</nuxt-link></h4>
+                <p class="item-content">
+                    {{data.summary}}
+                </p>
+                <div class="item-footer">
+                    <div class="info-left">
+                        <span>
+                            <i class="el-icon-location-outline"></i>
+                            {{data.cityName}}
+                        </span>
+                        <div class="userInfo">
+                            by
+                            <el-link type="info" :underline="false">
+                                <img class="avater" :src="$axios.defaults.baseURL+data.account.defaultAvatar" alt="">
+                                <span class="username">{{data.account.nickname}}</span>
+                            </el-link>
+                        </div>
+                        <span> <i class="el-icon-view"></i> {{data.watch | zerofill}}</span>
+                    </div>
+                    <span class="info-right">{{data.like | zerofill}}赞</span>
+                </div>
+            </div>
+        </el-col>
+    </el-row>
+    <el-row class="post-item" type="flex" justify="space-between" v-else>
+        <el-col :span="24">
+            <div class="item-text">
+                <h4 class="item-title"><nuxt-link to="#">{{data.title}}</nuxt-link></h4>
+                <p class="item-content">
+                    {{data.summary}}
+                </p>
+                <div class="item-images">
+                    <img class="item-img" :src="data.images.length>0?data.images[0]:'/img/default.jpg'" alt="">
+                    <img class="item-img" :src="data.images.length>0?data.images[1]:'/img/default.jpg'" alt="">
+                    <img class="item-img" :src="data.images.length>0?data.images[2]:'/img/default.jpg'" alt="">
+                </div>
+                <div class="item-footer">
+                    <div class="info-left">
+                        <span>
+                            <i class="el-icon-location-outline"></i>
+                            {{data.cityName}}
+                        </span>
+                        <div class="userInfo">
+                            by
+                            <el-link type="info" :underline="false">
+                                <img class="avater" :src="$axios.defaults.baseURL+data.account.defaultAvatar" alt="">
+                                <span class="username">{{data.account.nickname}}</span>
+                            </el-link>
+                        </div>
+                        <span> <i class="el-icon-view"></i> {{data.watch | zerofill}}</span>
+                    </div>
+                    <span class="info-right">{{data.like | zerofill}}赞</span>
+                </div>
+            </div>
+        </el-col>
+    </el-row>
+   </div>
+</template>
+
+<script>
+export default {
+    props:{
+        data:{
+            type:Object,
+            default:{}
+        }
+    },
+    filters:{
+        zerofill(value){
+
+            return value?value:0
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.post-item{
+    padding: 20px 0;
+    .item-img{
+        display: block;
+        width: 220px;
+        height: 150px;
+        object-fit: cover;
+    }
+    .item-text{
+        display: flex;
+        flex-direction: column;
+        justify-content:space-between;
+        height: 100%;
+        .item-title{
+            font-size: 18px;
+            font-weight: normal;
+            margin-bottom: 15px;
+            overflow: hidden;
+            text-overflow:ellipsis;
+            white-space: nowrap;
+            &:hover{
+                color: #409eff;
+            }
+        }
+        .item-content{
+            line-height: 24px;
+            flex: 1;
+            font-size: 14px;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 3;
+            overflow: hidden;
+            margin-bottom: 15px;
+        }
+        .item-images{
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 15px;
+        }
+        .item-footer{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            .info-left{
+                font-size: 12px;
+                display: flex;
+                color: #999;
+                display: flex;
+                align-items: center;
+            }
+            .info-right{
+                color: #409eff;
+            }
+        }
+    }
+    .userInfo{
+        display: flex;
+        align-items: center;
+        margin: 0 10px;
+        .avater{
+            margin: 5px;
+            width: 16px;
+            height: 16px;
+            vertical-align: middle;
+        }
+        .username{
+            color: #409eff;
+        }
+    }
+}
+
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,142 @@
 <template>
-  <div>旅游攻略</div>
+  <section class="container">
+      <el-row type="flex"  justify="space-between">
+        <!-- 侧边栏 -->
+        <el-col :span="7">
+          <div class="sidebar">
+            <!-- 城市列表 -->
+            <cityMenu :data="allMenuData" />
+
+            <!-- 推荐城市 -->
+            <div class="recommendCity">
+              <div class="recommendCity-title">
+                <span>推荐城市</span>
+              </div>
+              <img class="recommendCity-img" src="http://157.122.54.189:9093/images/pic_sea.jpeg">
+            </div>
+          </div>
+        </el-col>
+
+        <!-- 文章列表 -->
+        <el-col :span="17">
+          <div class="post-wrapper">
+            <!-- 城市搜索 -->
+            <div class="city-search">
+              <el-input v-model="cityName" placeholder="请输入想去的地方，比如：'广州'">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+              </el-input>
+              <div class="search-recommend">推荐: <span class="recommend">广州</span>
+                      <span class="recommend">上海</span>
+                      <span class="recommend">北京</span>
+              </div>
+            </div>
+            <!-- 标题 -->
+            <div class="post-title">
+              <el-row type="flex" justify="space-between" align="middle">
+                <h4 class="title">推荐攻略</h4>
+                <el-button type="primary" icon="el-icon-edit">写游记</el-button>
+              </el-row>
+            </div>
+            <!-- 攻略文章 -->
+            <div class="post-list">
+              <postList :data="postItem" v-for="(postItem,index) in allPostData" :key="index"/>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+  </section>
 </template>
 
 <script>
+import cityMenu from '@/components/post/cityMenu';
+import postList from '@/components/post/postList';
 export default {
+  components:{
+    cityMenu,
+    postList
+  },
+  data(){
+    return {
+      cityName:'',
+      allMenuData:[],
+      allPostData:[
+        {
+          images:[],
+          account:{}
+        }
+        ],
 
+      total:0
+    }
+  },
+  mounted(){
+      //获取城市菜单
+      this.$axios({
+          url:'/posts/cities',
+          method:'get'
+      }).then(res=>{
+          const {data}=res.data;
+          console.log(data);
+          this.allMenuData=data;
+      })
+
+      //获取推荐菜单
+      this.$axios({
+        url:'/posts',
+        method:'get',
+        // params:{
+        //   city:'广州'
+        // }
+      }).then(res=>{
+        const {data,total}=res.data;
+        console.log(data);
+        
+        this.allPostData=data;
+        this.total=total;
+      })
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container{
+  width:1000px;
+  margin:0 auto;
+}
+.sidebar{
+  width: 260px;
+  .recommendCity{
+    margin-top: 20px;
+    &-title{
+      padding-bottom: 10px;
+      border-bottom: 1px solid #dddddd;
+    }
+    &-img{
+      margin-top: 10px;
+      width: 100%;
+      object-fit: cover;
+    }
+  }
+}
+.post-wrapper{
+  margin-top: 20px;
+  .search-recommend{
+    padding: 10px 0;
+    font-size: 12px;
+    color: #666;
+    recommend{
+      margin-right:5px;
+    }
+  }
+  .post-title{
+    border-bottom: 1px solid #dddddd;
+    .title{
+      color: #409eff;
+      font-size: 18px;
+      font-weight: normal;
+      border-bottom: 2px solid #409eff;
+      line-height: 50px;
+    }
+  }
+}
 </style>
