9afb3802384f7713650df9bf13731134019655d8
afreshQ <1040044589@qq.com>
2019-11-19 14:43:14 +0800

    抽离评论模块

--- components/post/postComment.vue
+++ components/post/postComment.vue
@@ -1,8 +1,45 @@
 <template>
     <div class="comment">
-        <commentItem :data="item" v-for="(item,index) in commentData" :key="index"/>
-        <!-- 分页 -->
-        <div></div>
+
+            <div class="post-comment">
+                <h4 class="comment-title">评论</h4>
+                <el-input
+                class="input"
+                type="textarea"
+                :rows="2"
+                placeholder="说点什么把..."
+                v-model="form.content"
+                resize="none">
+                </el-input>
+                <el-row type="flex" justify="space-between" class="img-upload">
+                    <el-col>
+                        <el-upload
+                        name="files"
+                        :action="$axios.defaults.baseURL + '/upload'"
+                        list-type="picture-card"
+                        :on-success="coverUploaded"
+                        :on-preview="handlePictureCardPreview"
+                        :on-remove="handleRemove">
+                        <i class="el-icon-plus"></i>
+                        </el-upload>
+                        <el-dialog :visible.sync="dialogVisible">
+                            <img width="100%" class="upload-img" :src="dialogImageUrl" alt="">
+                        </el-dialog>
+                    </el-col>
+                    <el-col :span="3">
+                          <el-button type="primary" @click="handleSubmit">提交</el-button>
+                    </el-col>
+                </el-row>
+                <div class="no-comments" v-if="commentData.length===0">
+                                    暂无评论，赶紧抢占沙发！
+                </div>
+
+                <commentItem :data="item" v-for="(item,index) in commentData" :key="index" v-else/>    
+            </div>
+
+
+            <!-- 分页 -->
+            <div></div>
     </div>
 </template>
 
@@ -14,7 +51,17 @@ export default {
     },
     data(){
         return {
-            commentData:[]
+            commentData:[],
+
+            //图片预览组件
+            dialogImageUrl: '',
+            dialogVisible: false,
+
+            //评论表单
+            form:{
+                content:'',
+                pics:[]
+            },
         }
     },
 
@@ -32,10 +79,78 @@ export default {
             
             this.commentData=data;
         })
+    },
+
+    methods:{
+                // 文件上传成功时的钩子
+        coverUploaded(response){
+            this.form.pics.push(response[0]);
+        },
+        //图片删除时的钩子
+        handleRemove(file, fileList) {
+        console.log(file, fileList);
+            this.form.pics=fileList;
+        },
+
+        // 点击图片时的钩子
+        handlePictureCardPreview(file) {
+            this.dialogImageUrl = file.url;
+            this.dialogVisible = true;
+        },
+
+
+        //提交评论
+        handleSubmit(){
+            const post=this.postId
+            this.form={...this.form,post};
+            console.log(this.form);
+            
+            this.$axios({
+                url:'/comments',
+                method:'post',
+                headers:{
+                    'Content-Type':"application/json; charset=utf-8",
+                    Authorization:"Bearer "+this.$store.state.user.userInfo.token
+                },
+                data:this.form
+            }).then(res=>{
+                console.log(res);
+                
+            })
+        }
     }
 }
 </script>
 
 <style lang="less" scoped>
-
+.post-comment{
+    .comment-title{
+        font-size: 18px;
+        font-weight: normal;
+        margin-bottom: 20px;
+    }
+    .input{
+        margin-bottom: 10px;
+    }
+    .img-upload{
+        margin-bottom: 30px;
+    }
+    /deep/.el-upload{
+        width: 100px;
+        height: 100px;
+        line-height: 100px;
+    }
+    /deep/.el-upload-list__item-actions,
+    /deep/.el-upload-list__item{
+        width: 100px;
+        height: 100px;
+    }
+    .no-comments{
+        font-size: 14px;
+        color: #999;
+        padding: 30px;
+        text-align: center;
+        border: 1px dashed #dddddd;
+    }
+}
 </style>
--- pages/post/detialPost.vue
+++ pages/post/detialPost.vue
@@ -12,8 +12,8 @@
             </div>
             <!-- 文章详情渲染 -->
             <postRender :postData="postData"/>
-            <!-- 用户功能 -->
-             <div class="post-bar">
+            <!-- 用户交换功能 -->
+            <div class="post-bar">
                 <div class="icon"><i class="el-icon-chat-line-square"></i><p>评论({{postData.comments.length | zerofill}})</p></div>
                 <div class="icon"
                 @click="handleStar"
@@ -25,39 +25,8 @@
             </div>
 
             <!-- 文章评论 -->
-            <div class="post-comment">
-                <h4 class="comment-title">评论</h4>
-                <el-input
-                class="input"
-                type="textarea"
-                :rows="2"
-                placeholder="说点什么把..."
-                v-model="textarea"
-                resize="none">
-                </el-input>
-                <el-row type="flex" justify="space-between" class="img-upload">
-                    <el-col>
-                        <el-upload
-                        action="https://jsonplaceholder.typicode.com/posts/"
-                        list-type="picture-card"
-                        :on-preview="handlePictureCardPreview"
-                        :on-remove="handleRemove">
-                        <i class="el-icon-plus"></i>
-                        </el-upload>
-                        <el-dialog :visible.sync="dialogVisible">
-                            <img width="100%" class="upload-img" :src="dialogImageUrl" alt="">
-                        </el-dialog>
-                    </el-col>
-                    <el-col :span="3">
-                          <el-button type="primary">提交</el-button>
-                    </el-col>
-                </el-row>
-                <div class="no-comments" v-if="postData.comments.length===0">
-                                    暂无评论，赶紧抢占沙发！
-                </div>
-                <postComment :postId="postId" v-else/>
+            <postComment/>
 
-            </div>
         </el-col>
         <!-- 相关攻略 -->
         <el-col :span="7">
@@ -89,21 +58,12 @@ export default {
                 account:{}
             },
 
-            textarea:'',
-
-            //图片上传组件
-            dialogImageUrl: '',
-            dialogVisible: false,
-
             //相关文章数据
             recommendPostData:[]
         }
     },
+    //处理点击相关文章能跳到对应文章详情
     beforeRouteUpdate (to, from, next) {
-    // 在当前路由改变，但是该组件被复用时调用
-    // 举例来说，对于一个带有动态参数的路径 /foo/:id，在 /foo/1 和 /foo/2 之间跳转的时候，
-    // 由于会渲染同样的 Foo 组件，因此组件实例会被复用。而这个钩子就会在这个情况下被调用。
-    // 可以访问组件实例 `this`
         this.getDetailPost(to.query.id);
         next()
     },
@@ -117,6 +77,7 @@ export default {
     },
 
     methods:{
+        //获取当前文章数据
         getDetailPost(id){
             this.$axios({
                 url:'/posts',
@@ -174,13 +135,7 @@ export default {
                 this.$message.success(res.data.message);
             })
         },
-        handleRemove(file, fileList) {
-        console.log(file, fileList);
-        },
-        handlePictureCardPreview(file) {
-            this.dialogImageUrl = file.url;
-            this.dialogVisible = true;
-        }
+
     },
 
     filters:{
@@ -202,6 +157,7 @@ export default {
     display: flex;
     justify-content: center;
     .icon{
+        cursor: pointer;
         margin: 0 30px;
         display: flex;
         flex-direction: column;
@@ -218,31 +174,6 @@ export default {
     }
 }
 
-.post-comment{
-    .comment-title{
-        font-size: 18px;
-        font-weight: normal;
-        margin-bottom: 20px;
-    }
-    .input{
-        margin-bottom: 10px;
-    }
-    .img-upload{
-        margin-bottom: 30px;
-    }
-    /deep/.el-upload{
-        width: 100px;
-        height: 100px;
-        line-height: 100px;
-    }
-    .no-comments{
-        font-size: 14px;
-        color: #999;
-        padding: 30px;
-        text-align: center;
-        border: 1px dashed #dddddd;
-    }
-}
 .aside-title{
     font-weight: normal;
     font-size: 18px;
